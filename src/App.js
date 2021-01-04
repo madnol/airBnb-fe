3a588a52a5e7d5a9fb87960284fae52ffc4b11f1
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-04 17:06:16 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Home /> */}
-      {/* <Footer /> */}
+      <Home />
+      <Footer />
     </div>
   );
 }
--- src/HomeComponent/Home.jsx
+++ src/HomeComponent/Home.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import "./Home.scss";
+import MapIcon from "@material-ui/icons/Map";
 
 function Home() {
   return (
     <div>
-      <h1>This my home component</h1>
+      <p>300+ stays · Jan 3 - 9</p>
+      <h1>Stays in selected map area</h1>
+      <div className="home_top">
+        <div className="home_right">
+          <p>Cancellation flexibility</p>
+          <p>Type of place</p>
+          <p>Price</p>
+          <p>Instant Book</p>
+          <p>More filters</p>
+        </div>
+        <div className="home_left">
+          <MapIcon />
+          <p>Show map</p>
+        </div>
+      </div>
     </div>
   );
 }
